054d98c310ecacaf707d4e43a8f68cf6d8756a0d
Ochoa2in <102831684+Ochoa2in@users.noreply.github.com>
2022-04-01 14:32:22 -0700

    nuevo

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,8 +1,9 @@
 import streamlit as st 
 import pandas as pd
 st.title("Mi primer app")
+#las lienas eran para aprender a el primer ejericio 
 #st.button("dale click")
 #st.button("otro botón")
-df=pd.read_csv("https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv")
-st.write(df)
-st.map(df)
+#df=pd.read_csv("https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv")
+#st.write(df)
+#st.map(df)
