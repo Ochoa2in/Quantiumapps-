a91a4fd529946534b59e31b5f786f718aa0ea997
Ochoa2in <102831684+Ochoa2in@users.noreply.github.com>
2022-04-01 15:00:57 -0700

    w

--- Ejemplo.py
+++ Ejemplo.py
@@ -21,4 +21,4 @@ if click==True:
 num1=st.slider("elige un numero 1",0.0,100.0,50.00)
 num2=st.slider("elige el numero 2",0.0,100.0,50.00)
 suma=num1+num2
-st.write("la suma de,"num1,"y",num2,"es:",suma)
+st.write("la suma de",num1,"y",num2,"es:",suma)
