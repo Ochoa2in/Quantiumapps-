cd36bc846ecb407e7af0126ad16e865be2925d44
Ochoa2in <102831684+Ochoa2in@users.noreply.github.com>
2022-04-01 14:59:18 -0700

    f

--- Ejemplo.py
+++ Ejemplo.py
@@ -19,3 +19,6 @@ if click==True:
 #st.markdown("#titulo")
 #st.markdown("*este es una viñeta*")
 num1=st.slider("elige un numero 1",0.0,100.0,50.00)
+num2=st.slider("elige el numero 2",0.0,100.0,50.00)
+suma=num1+num2
+st.write("la suma de,"num1,"y",num2,"es": ;suma)
